46debcce1951d24786122c6a7cab83aef3a286ae
Andrei Gravonski <gravonskia@gmail.com>
2025-05-22 17:34:32 -0300

    projeto inicial criado com dependências básicas

---
+++ src/main/java/com/adotepet/adotepet/AdotepetApplication.java
@@ -0,0 +1,13 @@
+package com.adotepet.adotepet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotepetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adotepet/adotepet/AdotepetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adotepet.adotepet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotepetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
