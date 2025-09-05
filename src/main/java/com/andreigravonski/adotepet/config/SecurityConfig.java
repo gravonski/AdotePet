45ad96f58af1189834ad79876940e8aa2c478577
Andrei Gravonski <gravonskia@gmail.com>
2025-09-05 08:00:13 -0300

    feat(security): configuração do acesso público no Spring Security

--- src/main/java/com/andreigravonski/adotepet/config/SecurityConfig.java
+++ src/main/java/com/andreigravonski/adotepet/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/home", "/login").permitAll()
+                        .requestMatchers("/", "/home", "/login", "/denunciar", "/denunciar/salvar").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
