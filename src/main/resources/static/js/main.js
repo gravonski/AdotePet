a044f312bc1545748868492a0ccc21ae9795b054
Andrei Gravonski <gravonskia@gmail.com>
2025-10-05 23:00:33 -0300

    refactor(frontend): Centraliza lógica de confirmação de exclusão em event listener de JS

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,17 @@
-console.log("AdotePet JS: O script principal foi carregado com sucesso.");
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("AdotePet JS: O script principal foi carregado com sucesso.");
+    const deleteButtons = document.querySelectorAll('.btn-delete');
+
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+
+            const confirmed = confirm('Você tem certeza que deseja excluir este item?');
+
+            if (!confirmed) {
+                event.preventDefault();
+            }
+        });
+    });
+
+});
