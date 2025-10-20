ea9492fe1ce7b88e005243f917f9eaee7cb7d146
Andrei Gravonski <gravonskia@gmail.com>
2025-10-20 00:31:29 -0300

    fix(auth): Estabiliza autenticação com BCrypt e remove inicializador de usuário

--- src/main/java/com/andreigravonski/adotepet/AdotepetApplication.java
+++ src/main/java/com/andreigravonski/adotepet/AdotepetApplication.java
@@ -1,12 +1,8 @@
 package com.andreigravonski.adotepet;
 
-import com.andreigravonski.adotepet.service.ONGService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import com.andreigravonski.adotepet.service.ONGService;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 
 @EnableMethodSecurity
 @SpringBootApplication
@@ -16,11 +12,4 @@ public class AdotepetApplication {
 		SpringApplication.run(AdotepetApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ONGService ongService) {
-		return args -> {
-			// Chama nosso método para criar o admin
-			ongService.criarAdminUserSeNaoExistir();
-		};
-	}
 }
